8c7c218f6433a560b92c77ea771bafaee407c2da
VsevolodM95 <myemail>
2014-07-07 22:37:04 +0300

    7th of july

---
+++ tutorial_34.py
@@ -0,0 +1,18 @@
+#tutorial 34, Accessing Elements of Lists
+
+animals = ['bear', 'tiger', ' penguin', 'zebra']
+bear = animals[0] #first element of the list
+print "animals[0] = ", bear
+
+#
+#animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', #'platypus']
+#
+#    The animal at 1.           - python
+#    The third (3rd) animal.    - peacock
+#    The first (1st) animal.    - bear
+#    The animal at 3.           - kangaroo
+#    The fifth (5th) animal.    - whale
+#    The animal at 2.           - peacock
+#    The sixth (6th) animal.    - platypus
+#    The animal at 4.           - whale
+
